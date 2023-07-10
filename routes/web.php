e11e2478255ad7498d366e2211d049156cfdf2e2
Faysal Ahamed <miufaysal@live.com>
2023-07-10 16:37:15 +0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/admin');
-});
+Route::redirect('/', '/admin');
+Route::redirect('/filament/login', '/admin/login')->name('login');
